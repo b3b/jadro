7e28130333425f629c37dd3868f3acef67c2b4cd
b3b <ash.b3b@gmail.com>
2012-05-04 00:26:28 +0400

    runserver with --noreload option

--- run.py
+++ run.py
@@ -7,6 +7,6 @@ su = '/system/bin/su'
 python_executable = '/data/data/com.googlecode.pythonforandroid/files/python/bin/python'
 run = os.path.join(TOP_DIR, 'tools', 'run.sh')
 manage = os.path.join(TOP_DIR, 'manage.py')
-cmd = 'runserver'
+cmd = 'runserver --noreload'
 os.environ['LC_ALL'] = 'en_US.UTF8'
 os.execv(su, ['su', '-c', ' '.join(('sh', run, 'su', 'sh', run, python_executable, manage, cmd))])
