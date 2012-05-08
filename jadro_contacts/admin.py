624bb223c31b9564cc7671008eb77181c942946c
b3b <ash.b3b@gmail.com>
2012-05-08 19:15:07 +0400

    send_sms admin action

--- jadro_contacts/admin.py
+++ jadro_contacts/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.contrib.admin.util import flatten_fieldsets
+from django.contrib import messages
+from django.template import Context, Template
+from django.template.response import TemplateResponse
+from django import forms
 from django.forms.models import fields_for_model
+from django.conf import settings
 from models import RawContact, Phone, Photo, GroupMembership
+
+droid = settings.DROID_CONNECTION
 
 class ReadOnlyAdmin(admin.ModelAdmin):
     def get_readonly_fields(self, request, obj=None):
@@ -28,6 +35,14 @@ class PhotoInline(DataInline):
 class GroupMembershipInline(DataInline):
     model = GroupMembership
 
+class AdminActionForm(forms.Form):
+    _selected_action = forms.CharField(widget=forms.MultipleHiddenInput)
+    action = forms.CharField(widget=forms.HiddenInput())
+    post = forms.CharField(widget=forms.HiddenInput(), initial='yes')
+
+class SendSmsForm(AdminActionForm):
+    message = forms.CharField(widget=forms.Textarea)
+
 class RawContactAdmin(ContactAdmin):
     fields = ['display_name', 'times_contacted', 'last_time_contacted']
     list_display = ['display_name', 'display_phone', 'display_photo']
@@ -35,6 +50,7 @@ class RawContactAdmin(ContactAdmin):
     ordering = ['-last_time_contacted']
     search_fields = ['display_name']
     inlines = [ PhoneInline, PhotoInline, GroupMembershipInline ]
+    actions = ['send_sms']
 
     def display_photo(self, obj):
         photos = obj.photos
@@ -46,6 +62,32 @@ class RawContactAdmin(ContactAdmin):
         phones = obj.phones
         return phones and phones[0] or ''
     display_phone.short_description = 'phone'
-       
-admin.site.register(RawContact, RawContactAdmin)
 
+    def send_sms(modeladmin, request, queryset):
+        if request.POST.get('post'):
+            message_template = SendSmsForm(request.POST).data['message']
+            for contact in queryset:
+                message = Template(message_template).render(Context({'contact': contact}))
+                phone = contact.phones and "%s" % (contact.phones[0])
+                if phone:
+                    result = droid.smsSend(phone, message)
+                    if not result.error:
+                        messages.info(request, 'SMS message sended to %(contact)s (%(phone)s).' % {
+                                'contact': contact, 'phone': phone})
+                    else:
+                        messages.error(request, 'Error sending SMS message to %(contact)s (%(phone)s).' % {
+                                'contact': contact, 'phone': phone})
+                else:
+                    messages.error(request, 'Error sending SMS message to %(contact)s: no phone.' % {
+                            'contact': contact})
+            return None
+        return TemplateResponse(request, 'jadro_contacts/send_sms.html', {
+                'contacts': queryset, 'form': SendSmsForm(initial={
+                        'action': 'send_sms',
+                        '_selected_action': request.POST.getlist('_selected_action')})})
+
+    def get_actions(self, request):
+        actions = super(RawContactAdmin, self).get_actions(request)
+        return dict([(k,v) for k,v in actions.items() if k in self.actions])
+
+admin.site.register(RawContact, RawContactAdmin)
